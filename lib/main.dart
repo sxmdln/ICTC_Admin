9a239a0f04832360e294e5317de7cdb717c473e8
Juanabell Abiada <89516778+saddcow@users.noreply.github.com>
2024-02-19 10:10:03 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ictc_admin/main_screen.dart';
 import 'package:ictc_admin/pages/auth/login_page.dart';
 import 'package:ictc_admin/pages/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:ictc_admin/pages/programs_page.dart';
 // import 'package:responsive_builder/responsive_builder.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'firebase_options.dart';
@@ -75,7 +76,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: LoginPage(),
+      body: ProgramsPage(),
     );
   }
 }
