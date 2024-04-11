693abad7e739b71533f2759e0195f22f8256a92d
sxmdln <57649092+sxmdln@users.noreply.github.com>
2024-04-11 11:43:30 +0800

    Added "Expenses" as a new tab and table

---
+++ lib/pages/expenses/expenses_form.dart
@@ -0,0 +1,142 @@
+import 'dart:developer';
+import 'package:intl/intl.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ExpensesForm extends StatefulWidget {
+  const ExpensesForm({super.key, this.expense});
+
+  final Object? expense;
+
+  @override
+  State<ExpensesForm> createState() => _ExpensesFormState();
+}
+
+class _ExpensesFormState extends State<ExpensesForm> {
+  TextEditingController datePickerController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: datePickerController,
+            readOnly: true,
+            decoration: const InputDecoration(
+              alignLabelWithHint: true,
+              hintText: "Date of Expense",
+              hintStyle: TextStyle(fontSize: 14,height: 2),
+              filled: true,
+              isDense: true,
+              prefixIcon: Icon(Icons.calendar_month, size: 20),
+            ),
+            onTap: () => onTapFunction(context: context),
+          ),
+          
+                SizedBox(height: 10),
+    
+          CupertinoTextFormFieldRow(
+            prefix: const Row(
+              children: [
+                Text("Total Cost",
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400)),
+                SizedBox(width: 27),
+              ],
+            ),
+            // padding: EdgeInsets.only(left: 90),
+            placeholder: "Enter total cost of expense.",
+            placeholderStyle: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Colors.black45,
+            ),
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Colors.black87,
+            ),
+            decoration: BoxDecoration(
+              // border: ,
+              border: Border.all(
+                color: Colors.black87,
+                width: 0.5,
+              ),
+              borderRadius: BorderRadius.circular(18),
+              // prefixIcon: Icon(Icons.person)
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              // Expanded(child: SizedBox(child: cancelButton())),
+              if (widget.expense != null)
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(child: deleteButton()),
+                ),
+              const SizedBox(width: 10),
+              Expanded(
+                flex: 1,
+                child: SizedBox(child: saveButton()),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget saveButton() {
+    return FilledButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return Colors.greenAccent;
+          }
+          return Colors.green;
+        }),
+      ),
+      onPressed: () {},
+      child: const Text(
+        "Save",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget deleteButton() {
+    return FilledButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.white70;
+            }
+            return const Color.fromARGB(255, 226, 226, 226);
+          }),
+        ),
+        onPressed: () {},
+        child: const Text(
+          "Delete",
+          style: TextStyle(color: Colors.black87),
+        ));
+  }
+
+  onTapFunction({required BuildContext context}) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      lastDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      initialDate: DateTime.now(),
+    );
+    if (pickedDate == null) return;
+    datePickerController.text = DateFormat('yyyy-MM-dd').format(pickedDate);
+  }
+}
