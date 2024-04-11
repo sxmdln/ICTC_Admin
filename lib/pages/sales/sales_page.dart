d7d8a75903dc6866f483816152f4c6091ab6366e
sxmdln <57649092+sxmdln@users.noreply.github.com>
2024-04-11 11:43:38 +0800

    Added "Sales" as a new tab and table

---
+++ lib/pages/sales/sales_page.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:data_table_2/data_table_2.dart';
+import 'package:ictc_admin/models/seeds.dart';
+import 'package:ictc_admin/models/sale.dart';
+import 'package:ictc_admin/pages/sales/sales_form.dart';
+
+class SalesPage extends StatefulWidget {
+  const SalesPage({super.key});
+
+  @override
+  State<SalesPage> createState() => _SalesPageState();
+}
+
+class _SalesPageState extends State<SalesPage> {
+  late List<Sale> sales;
+
+  @override
+  void initState() {
+    // TODO: implement initState for populating the table with data from the backend
+    sales = Seeds.sales;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          flex:1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                // margin: EdgeInsets.symmetric(horizontal: 100),
+                padding: const EdgeInsets.only(right: 5, bottom: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    addButton(),
+                  ],
+                ),
+              ),
+              buildDataTable(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildDataTable() {
+    return Expanded(
+      child: DataTable2(
+        showCheckboxColumn: false,
+        showBottomBorder: true,
+        horizontalMargin: 30,
+        isVerticalScrollBarVisible: true,
+        columns: const [
+          DataColumn2(
+              label: Text(
+            'Name of Trainer',
+          )),
+          DataColumn2(
+              label: Text(
+            'Schedule',
+          )),
+          DataColumn2(
+              label: Text(
+            'Total Students',
+          )),
+          DataColumn2(
+              label: Text(
+            'Total Sale',
+          )),
+          DataColumn2(
+              label: Text(
+            'Total Discount',
+          )),
+          DataColumn2(
+              label: Text(
+            'Actions',
+          )),
+        ],
+        rows: sales.map((e) => buildRow(e)).toList(),
+      ),
+    );
+  }
+
+  DataRow2 buildRow(Sale sale) {
+    return DataRow2(onSelectChanged: (selected) {}, cells: [
+      DataCell(Text(sale.toString())),
+      DataCell(Text(sale.schedule)),
+      DataCell(Text(sale.totalStudents.toString())),
+      DataCell(Text(sale.saleTotal.toString())),
+      DataCell(Text(sale.discountTotal.toString())),
+      
+
+
+      DataCell(Row(
+        children: [
+          editButton(),
+        ],
+      )),
+    ]);
+  }
+
+  Widget editButton() {
+    return TextButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return editDialog();
+            },
+          );
+        },
+        child: const Row(
+          children: [
+            Icon(
+              Icons.edit,
+              size: 20,
+              color: Color(0xff153faa),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Text("Edit"),
+          ],
+        ));
+  }
+
+
+  Widget editDialog() {
+    return AlertDialog(
+      // shape: const RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.all(Radius.circular(30))),
+      contentPadding: EdgeInsets.only(left: 20, right: 30, top: 40),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: FractionalOffset.topRight,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.clear),
+            ),
+          ),
+          const Text(
+            "Edit a Sale",
+            style: TextStyle(
+                color: Colors.black87,
+                fontSize: 24,
+                fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+      content: Flexible(
+        flex: 2,
+        child: SizedBox(
+          width: 380,
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: const Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SalesForm(sale: true),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget addButton() {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return addDialog();
+          },
+        );
+      },
+      // materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Container(
+        constraints: const BoxConstraints(
+            maxWidth: 100, minHeight: 36.0), // min sizes for Material buttons
+        alignment: Alignment.center,
+        child: const Row(children: [
+          Icon(
+            CupertinoIcons.add,
+            size: 20,
+            color: Colors.white,
+          ),
+          SizedBox(width: 6),
+          Text(
+            'Add a Sale',
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+        ]),
+      ),
+    );
+  }
+
+  
+  Widget addDialog() {
+    return AlertDialog(
+      // shape: const RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.all(Radius.circular(30))),
+      contentPadding: EdgeInsets.only(left: 20, right: 30, top: 40),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: FractionalOffset.topRight,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.clear),
+            ),
+          ),
+          const Text(
+            "Add a Sale",
+            style: TextStyle(
+                color: Colors.black87,
+                fontSize: 24,
+                fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+      content: Flexible(
+        flex: 1,
+        child: SizedBox(
+          width: 380,
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: const Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SalesForm(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
